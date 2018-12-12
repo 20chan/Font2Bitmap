02ad1ab7487ad779d78eefc61bf3a6454910031f
phillyai <sonagi.philly@gmail.com>
2018-12-12 17:36:25 +0900

    8x8 비트맵으로 구현

--- FontToBitmap/Program.cs
+++ FontToBitmap/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
 
 namespace FontToBitmap
 {
@@ -10,6 +13,33 @@ namespace FontToBitmap
     {
         static void Main(string[] args)
         {
+            var chars = "abcdefghijklmnopqrstuvwyz";
+            var result = new List<string[]>();
+            foreach (var c in chars)
+            {
+                var font = new Font("MS Gothic", 9);
+                var size = TextRenderer.MeasureText(c.ToString(), font);
+                var img = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
+                var g = Graphics.FromImage(img);
+                g.Clear(Color.White);
+                g.DrawString(c.ToString(), font, Brushes.Black, new Point(0, 0));
+
+                var builder = new StringBuilder();
+                for (int y = 2; y <= 2 + 8; y++)
+                {
+                    for (int x = 0; x <= 0 + 8; x++)
+                    {
+                        var color = img.GetPixel(x, y);
+                        int mean = (color.R + color.G + color.B) / 3;
+                        builder.Append(mean > 150 ? "□" : "■");
+                    }
+                    builder.AppendLine();
+                }
+
+                result.Add(builder.ToString().Split());
+                Console.WriteLine(builder.ToString());
+            }
+            Console.Read();
         }
     }
 }
